3dc7acc3c6a3eef6233dd0d93a259d4c767a6044
xanroaroff@gmail.com <xanroaroff@gmail.com>
2024-12-09 15:12:22 +0300

    Add project files.

---
+++ consoletest/Item.cs
@@ -0,0 +1,78 @@
+﻿
+using consoletest;
+using System.Collections;
+
+public class Item : IEnumerable
+{
+    private static int NEXT_ID = 0;
+    public int Id { get; }
+    public string Name { get; }
+    public decimal Price { get; }
+    public string Description { get; }
+
+    public delegate void ShowItemHandler(ItemEventArgs e);
+    public event ShowItemHandler DisplayItem;
+    public Item()
+    {
+    }
+    public Item(string name, decimal price, string description)
+    {
+        this.Id = ++NEXT_ID;
+        this.Name = name;
+        this.Price = price;
+        this.Description = description;
+    }
+    List<Item> Items { get; set; } = new List<Item>();
+
+    public void Add(Item item)
+    {
+        Items.Add(item);
+
+        DisplayItem?.Invoke(new ItemEventArgs(item, $"\nПоступил товар с параметрами: " +
+            $"\nИД: {item.Id} " +
+            $"\nНаименование: {item.Name}" +
+            $"\nЦена:{item.Price}" +
+            $"\nОписание: {item.Description}"));
+    }
+    public void Remove(int id)
+    {
+        var RemovingItem = Items.FirstOrDefault(x => x.Id == id);
+
+        if (RemovingItem == null)
+        {
+            Console.WriteLine($"Item with ID {id} not found.");
+            return;
+        }
+        Items.Remove(RemovingItem);
+        DisplayItem?.Invoke(new ItemEventArgs(RemovingItem, $"\nУдален товар с параметрами: " +
+            $"\nИД: {RemovingItem.Id}" +
+            $"\nНаименование: {RemovingItem.Name}" +
+            $"\nЦена: {RemovingItem.Price}" +
+            $"\nОписание: {RemovingItem.Description}"));
+    }
+    public void Edit(int editedItemId, Item newitem)
+    {
+        var SelectedItem = Items.FirstOrDefault(x => x.Id == editedItemId);
+
+        if (SelectedItem == null)
+        {
+            Console.WriteLine($"Item with ID {editedItemId} not found.");
+            return;
+        }
+
+        DisplayItem?.Invoke(new ItemEventArgs(SelectedItem, $"\nИзменен товар с параметрами: " +
+            $"\nИД: {SelectedItem.Id}" +
+            $"\nНаименование: {SelectedItem.Name}" +
+            $"\nЦена: {SelectedItem.Price}" +
+            $"\nОписание: {SelectedItem.Description}" +
+            $"\n\nНа товар с параметрами: " +
+            $"\nИД: {newitem.Id}" +
+            $"\nНаименование: {newitem.Name}" +
+            $"\nЦена:{newitem.Price}" +
+            $"\nОписание: {newitem.Description}"));
+        SelectedItem = newitem;
+    }
+
+    public IEnumerator<Item> GetEnumerator() => Items.GetEnumerator();
+    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+}
---
+++ consoletest/Program.cs
@@ -0,0 +1,40 @@
+﻿using consoletest;
+
+Item item = new Item();
+
+item.DisplayItem += DisplayMessage;
+
+
+item.Add(new Item("Item 1", 23.1m, "Desc 1"));
+item.Remove(1);
+
+item.Add(new Item("Item 1", 23.1m, "Desc 1"));
+item.Edit(2, new Item("Edited Item", 24.1m, "Desc for edited"));
+
+void DisplayMessage(ItemEventArgs e)
+{
+    Console.WriteLine(e.Message);
+    Console.ForegroundColor = ConsoleColor.Yellow;
+
+    bool Completed = true;
+    bool OperationDone = false;
+
+    foreach (var i in item)
+    {
+        Completed = true;
+        if (Completed == true && OperationDone == false)
+        {
+            Console.WriteLine("\nТекущий товар:");
+            OperationDone = true;
+        }
+        Console.WriteLine($"Ид_товара: {i.Id} " +
+                          $"\nНаименование товара: {i.Name} " +
+                          $"\nЦена товара: {i.Price} " +
+                          $"\nОписание товара: {i.Description}" +
+                          $"\n");
+    }
+
+    Console.ForegroundColor = ConsoleColor.Gray;
+}
+
+
